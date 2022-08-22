085983f0df05f332f6b2b4be16f02ad62a65cbcd
Grace Bunardi <gbunardi@hotmail.com>
2022-08-22 12:09:11 +0800

    added in photo onto list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,7 @@ class List < ApplicationRecord
   # A list must have a unique name.
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
 end
